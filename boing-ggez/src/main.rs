aa9f59efa31273dde47b402347c533ea675d440f
Saverio Miroddi <saverio.pub2@gmail.com>
2022-07-06 20:01:52 +0200

    Boing: Make fullscreen, using True mode

--- boing-ggez/src/main.rs
+++ boing-ggez/src/main.rs
@@ -13,7 +13,7 @@ mod state;
 use std::env;
 use std::path::PathBuf;
 
-use ggez::{event, GameResult};
+use ggez::{event, graphics::Rect, Context, GameError, GameResult};
 
 use global_state::GlobalState;
 
@@ -45,18 +45,84 @@ fn get_resource_dirs() -> Vec<PathBuf> {
         .collect()
 }
 
+fn configure_window_and_viewport(context: &mut Context) -> Result<(), GameError> {
+    // Attempted workaround (see below)
+    let (screen_width, screen_height) = (800., 600.);
+
+    ggez::graphics::set_drawable_size(context, screen_width, screen_height)?;
+
+    // Broken; returns 800x600
+    // let (screen_width, screen_height) = ggez::graphics::drawable_size(context);
+
+    let screen_ratio = screen_width / screen_height;
+    let viewport_ratio = WINDOW_WIDTH / WINDOW_HEIGHT; // game area ratio
+
+    let (viewport_width, viewport_height) = if screen_ratio > viewport_ratio {
+        // Screen is wider; keep the height, but stretch the width.
+        (screen_height * viewport_ratio, screen_height)
+    } else {
+        // screen is taller; keep the width, but stretch the height.
+        // we divide instead of multiplying because the ratio itself is calculated on the inverse
+        // relationship between W/H.
+        (screen_width, screen_width / viewport_ratio)
+    };
+
+    let tot_border_width = screen_width - viewport_width;
+    let tot_border_height = screen_height - viewport_height;
+
+    /*
+    Extremely confusing.
+
+    # Drawable size set to 1920x1080
+
+    SS:(1920.0, 1080.0)
+    VR:Rect { x: -60.0, y: -0.0, w: 1800.0, h: 1080.0 }
+
+    - the rendering of the viewport is around 1/4th of what it should be
+    - there is display corruption (viewport displayed multiple times, in different sizes and
+      locations)
+
+    Other viewport coordinates (e.g. Rect::new(0.,0.,viewport_width + tot_border_width,viewport_height + tot_border_height))
+    still cause corruption.
+
+    # Drawable size set to 800x600
+
+    With this Rect, there is no corruption, however, there are vertical bars to the side, instead
+    of the top:
+
+        VR:Rect { x: 0.0, y: 0.0, w: 800.0, h: 480.0 }
+    */
+
+    let viewport_rect = Rect::new(
+        -tot_border_width / 2.,
+        -tot_border_height / 2.,
+        viewport_width,
+        viewport_height,
+    );
+
+    println!("SS:{:?}", (screen_width, screen_height));
+    println!("VR:{:?}", viewport_rect);
+
+    ggez::graphics::set_screen_coordinates(context, viewport_rect)
+}
+
 fn main() -> GameResult {
     let resource_dirs = get_resource_dirs();
 
     let mut context_builder = ggez::ContextBuilder::new(GAME_ID, AUTHOR)
         .window_setup(ggez::conf::WindowSetup::default().title(WINDOW_TITLE))
-        .window_mode(ggez::conf::WindowMode::default().dimensions(WINDOW_WIDTH, WINDOW_HEIGHT));
+        .window_mode(
+            ggez::conf::WindowMode::default().fullscreen_type(ggez::conf::FullscreenType::True),
+        );
 
     for dir in resource_dirs {
         context_builder = context_builder.add_resource_path(dir);
     }
 
     let (mut context, event_loop) = context_builder.build()?;
+
+    configure_window_and_viewport(&mut context)?;
+
     let mut state = GlobalState::new(&mut context);
 
     state.play_music(&mut context)?;
